49428a4c1dcc818e98cd24f6289250928b8ddaa2
Patrick Bower <patrickbower@Patricks-MacBook-Pro.local>
2018-01-16 17:22:58 +0000

    chore (build) - add to GitHub pages host
    Initial deployment

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-    url: 'http://api.openweathermap.org/data/2.5/forecast',
+    url: 'https://api.openweathermap.org/data/2.5/forecast',
     key: '72a1c48d580876d05a14f91b21acc69d'
 };
 
